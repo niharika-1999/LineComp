9521d6c870b8b61707cec3e3f774b7df1c5fdc80
niharika-1999 <niharika.rao@ymedialabs.com>
2021-08-19 18:06:43 +0530

    Added:Equality of the line

--- src/com/bridgelabz/LineCompone.java
+++ src/com/bridgelabz/LineCompone.java
@@ -4,11 +4,19 @@ import java.lang.Math.*;
 public class LineCompone {
 	public static void main(String[] args) {
 		System.out.println("Welcome to line Comparision Computation");
+		
 		int x1,x2,y1,y2;
 		double length;
 		x1=2;y1=2;x2=4;y2=4;
 		length=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 		System.out.println("length of line "+"("+x1+","+y1+"),"+"("+x2+","+y2+")=>"+length);
+		
+		if((x1/x2 == y1/y2))
+			System.out.println("Lines are equal");
+		else
+			System.out.println("Lines are not equal");
+			
+		
 	}
 
 }
