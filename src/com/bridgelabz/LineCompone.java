c7b1bec58e46bac5561f2fb70fdbdee13a1d724a
niharika-1999 <niharika.rao@ymedialabs.com>
2021-08-20 10:24:20 +0530

    Added:Comparing two Lines

--- src/com/bridgelabz/LineCompone.java
+++ src/com/bridgelabz/LineCompone.java
@@ -5,16 +5,34 @@ public class LineCompone {
 	public static void main(String[] args) {
 		System.out.println("Welcome to line Comparision Computation");
 		
-		int x1,x2,y1,y2;
-		double length;
+		int x1,x2,y1,y2,a1,a2,b1,b2;
+		double length,length1;		
 		x1=2;y1=2;x2=4;y2=4;
+		a1=6;a2=6;b1=8;b2=8;
 		length=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+		length1=Math.sqrt((a2-a1)*(a2-a1)+(b2-b1)*(b2-b1));
 		System.out.println("length of line "+"("+x1+","+y1+"),"+"("+x2+","+y2+")=>"+length);
 		
 		if((x1/x2 == y1/y2))
 			System.out.println("Lines are equal");
 		else
 			System.out.println("Lines are not equal");
+		
+		if (length>length1)
+			System.out.println("First length is greater than second ");
+		else if(length<length1)
+			System.out.println("First length is lesser than second");
+		else
+			System.out.println("Both the lengths are equal");
+			
+		
+	
+		
+		
+		
+
+		
+		
 			
 		
 	}
